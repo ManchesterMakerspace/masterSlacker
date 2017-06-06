16228097e7587e564ae13148c79c68891db4abcf
Paul Beaudet <inof8or@gmail.com>
2017-06-05 20:04:59 -0400

    Stop spelling things wrong

--- masterSlacker.js
+++ masterSlacker.js
@@ -138,7 +138,7 @@ var socket = {                                                         // socket
                 client.on('channelMsg', slack.channelMsg(client.id));         // messages to channels outside of default one
                 client.on('disconnect', bot.disconnect(client.id));           // remove service from service array on disconnect
             } else {                                                          // in case token was wrong or name not provided
-                slack.send(ONSELF)('Rejected socket connection: ' + client.id);
+                slack.send(ONESELF)('Rejected socket connection: ' + client.id);
                 client.on('disconnect', function(){
                     slack.send(ONESELF)('Rejected socket disconnected: ' + client.id);
                 });
