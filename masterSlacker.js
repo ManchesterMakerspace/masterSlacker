427c6819d6eb1c28281bf5f5238dff011708ab89
Paul Beaudet <inof8or@gmail.com>
2017-10-08 22:10:12 -0400

    Testing out slash commands

--- masterSlacker.js
+++ masterSlacker.js
@@ -201,7 +201,34 @@ var socket = {                                                         // socket
     }
 };
 
-var server = require('http').createServer();
-socket.listen(server); // listen and handle socket connections
-server.listen(process.env.PORT);
+var route = {
+    santa: function(){
+        return function(req, res){
+            if(req.body){
+                res.status(200).send('OK');res.end();             // ACK notification
+                console.log(JSON.stringify(req.body, null, 4));
+            }
+        };
+    }
+};
+
+var serve = {                                                // handles express server setup
+    express: require('express'),                             // server framework library
+    parse: require('body-parser'),                           // middleware to parse JSON bodies
+    theSite: function(){                                     // method call to serve site
+        serve.app = serve.express();                         // create famework object
+        var http = require('http').Server(serve.app);        // http server for express framework
+        serve.app.use(serve.parse.json());                   // support JSON bodies
+        serve.app.use(serve.parse.urlencoded({extended: true})); // idk, something was broken maybe this fixed it
+        // serve.app.use('/', serve.express.static(path.join(__dirname, 'public'))); // serve static files for site resources
+        serve.router = serve.express.Router();               // create express router object to add routing events to
+        serve.router.post('/santa', route.santa());          // Don't ask
+        serve.app.use(serve.router);                         // get express to user the routes we set
+        return http;
+    }
+};
+
+var http = serve.theSite();
+socket.listen(http); // listen and handle socket connections
+http.listen(process.env.PORT);
 slack.init();
